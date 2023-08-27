95cef4f70d928d4bad0924b5309d95c60958272b
GenesisDesigns <140233360+GenesisDesigns@users.noreply.github.com>
2023-08-26 23:04:37 -0500

    Add files via upload

---
+++ Day 3/Day3_Concat_v1.py
@@ -0,0 +1,10 @@
+#Title: Replit Day  - Day3 - Concat...What- v1
+
+myFood = input("What's the food that you like?: ").title()
+plantType  = input("What type of plant do you like?: ").title()
+cookingMethod = input("What type of cooking method do you like to use?: ")
+burnedFood = input("What word describes burned food?: ")
+diyItem = input("What is a name of a DIY item?: ")
+
+print("MENU")
+print(cookingMethod, myFood, "with", burnedFood, plantType, "on a bed of", diyItem)
