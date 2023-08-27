15f9c4811039f7b995fbf3341e20c02fb705c15c
GenesisDesigns <140233360+GenesisDesigns@users.noreply.github.com>
2023-08-26 23:24:44 -0500

    Add files via upload

---
+++ Day 5/Day5_If_Else_v1.py
@@ -0,0 +1,25 @@
+#Title: Replit Day 5 - If This Else That v1
+
+print("------- Let Me Guess Who You Are -------")
+print()
+spiderMan = input("Do you like 'hanging around'?: ").capitalize()
+korg = input("Do you have a 'gravelly' voice?:" ).capitalize()
+captainMarvel = input("Do you often feel 'Marvelous'?: ").capitalize()
+
+print()
+
+print("Marvel Movie Character Creator")
+print("--")
+
+if spiderMan =="Yes":
+  print("Aha! You're Spider-Man! Hi!")
+elif korg == "Yes":
+  print("Aha! You're Korg! Hi!")
+elif captainMarvel == "Yes":
+  print("Aha! You're Captain Marvel! Hi!")
+else:
+  print("Aww, I don't know who you are!")
+
+
+
+  
