5e0bcc802d92a666814aab4fa28dcafe956c7d62
GenesisDesigns <140233360+GenesisDesigns@users.noreply.github.com>
2023-08-26 23:01:27 -0500

    Update and rename Day2_InputVariables_v2.py to Day2_InputVariables_v1.py

---
+++ Day 2/Day2_InputVariables_v1.py
@@ -0,0 +1,13 @@
+#Title: Replit Day  - Day2 - Input And Variables - v1
+
+myName = input("What's your name?: ").title()
+myFood = input("What's your favorite food?: ")
+myMusic = input("What's your favorite music?: ")
+myHouse = input("Where do you live?: ")
+print()
+
+print("Your name is?: ", myName)
+print("You're probably hungry for?: ", myFood)
+print("and you're definitely getting your groove on to?: ", myMusic)
+print("living and staying in?: ", myHouse)
+                                                                         
