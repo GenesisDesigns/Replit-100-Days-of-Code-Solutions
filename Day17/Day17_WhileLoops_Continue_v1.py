b8692074382079693d308ab33816ccc0efe112df
GenesisDesigns <140233360+GenesisDesigns@users.noreply.github.com>
2023-09-04 18:32:08 -0500

    Add files via upload

---
+++ Day17/Day17_WhileLoops_Continue_v1.py
@@ -0,0 +1,13 @@
+#Title: Replit Day  - Day17 - WhileLopps Continue - Lets Cheat Continue - v1
+
+
+player1_score = 0
+player2_score = 0
+choices = ['R', 'P', 'S']
+
+while player1_score <= 3 or player2_score <= 3:
+    print()
+    player1_choice = input("Player1, please choose (R, P, S): ")
+    player1_choice = player1_choice.upper()
+
+
