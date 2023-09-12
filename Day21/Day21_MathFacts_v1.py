5209a8caed2fcfd1353436785d427f11b2d0b183
GenesisDesigns <140233360+GenesisDesigns@users.noreply.github.com>
2023-09-11 19:56:13 -0500

    Update Day21_MathFacts_v1.py

--- Day21/Day21_MathFacts_v1.py
+++ Day21/Day21_MathFacts_v1.py
@@ -8,3 +8,14 @@ multiples = int(input("Name your multiples: "))
 start = 1
 stop = 5
 score = 0
+
+for i in range(start, stop):
+    correctAnswer = i * multiples
+    print()
+    yourAnswer = int(input(f"{i} x {multiples} = "))
+    print()
+    if yourAnswer == correctAnswer:
+        print("Great!")
+        score += 1
+    else:
+        print(f"Not quite. The correct answer is: {correctAnswer}")
