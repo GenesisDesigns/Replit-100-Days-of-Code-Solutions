647f65660479d6c5fbb975e0ad4eb88d2d72c286
GenesisDesigns <140233360+GenesisDesigns@users.noreply.github.com>
2023-09-14 13:15:40 -0500

    Update Day24_RollDice_v1.py

--- Day24/Day24_RollDice_v1.py
+++ Day24/Day24_RollDice_v1.py
@@ -4,3 +4,20 @@ print("-----| Infinity Dice Game |------")
 print()
 
 import random
+
+def infinityDice():
+  while True:
+    numSides = int(input("How many sides?: "))
+    randomSide = random.randint(1, 10)
+    if numSides == randomSide:
+      tryAgain = input("Correct! Try again? (Y/N): ")
+      if tryAgain.upper() == "Y":
+        continue
+      else:
+        exit()
+    elif numSides > randomSide:
+      print("Sides are lower, try again")
+      continue
+    else:
+      print("Sides are are higher, try again")
+      continue
