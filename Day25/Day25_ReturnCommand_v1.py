d775f2e945ecc33c2934e5f143f7d8d1880327ab
GenesisDesigns <140233360+GenesisDesigns@users.noreply.github.com>
2023-09-15 22:09:29 -0500

    Update Day25_ReturnCommand_v1.py

--- Day25/Day25_ReturnCommand_v1.py
+++ Day25/Day25_ReturnCommand_v1.py
@@ -9,3 +9,10 @@ def rollDice():
   dice1 = random.randint(1, 6)
   dice2 = random.randint(1, 8)
   return dice1 * dice2
+
+while True:
+  charName = input("Character Name: ").title()
+  print(charName)
+  healthBar = rollDice()
+  print(f"Character's health is (HP): {healthBar}")
+  tryAgain = input("Name another character? (Y/N): ").capitalize()
