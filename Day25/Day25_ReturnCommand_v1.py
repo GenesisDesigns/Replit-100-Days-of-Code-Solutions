2556e88129afdcce596673358744ef745a5b8597
GenesisDesigns <140233360+GenesisDesigns@users.noreply.github.com>
2023-09-15 22:09:48 -0500

    Update Day25_ReturnCommand_v1.py

--- Day25/Day25_ReturnCommand_v1.py
+++ Day25/Day25_ReturnCommand_v1.py
@@ -1,14 +1,17 @@
 #Title: Replit Day  - Day25 - ReturnCommand - Send It Back - v1
 
+
 print("-----| character Stat Generator | ------")
 print("-------------------------------------------")
 
 import random
 
+
 def rollDice():
   dice1 = random.randint(1, 6)
   dice2 = random.randint(1, 8)
   return dice1 * dice2
+
 
 while True:
   charName = input("Character Name: ").title()
