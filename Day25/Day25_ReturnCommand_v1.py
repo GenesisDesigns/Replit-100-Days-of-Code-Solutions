05635a8b32c9beada74f1c03134154ecd7d45006
GenesisDesigns <140233360+GenesisDesigns@users.noreply.github.com>
2023-09-15 22:09:19 -0500

    Update Day25_ReturnCommand_v1.py

--- Day25/Day25_ReturnCommand_v1.py
+++ Day25/Day25_ReturnCommand_v1.py
@@ -4,3 +4,8 @@ print("-----| character Stat Generator | ------")
 print("-------------------------------------------")
 
 import random
+
+def rollDice():
+  dice1 = random.randint(1, 6)
+  dice2 = random.randint(1, 8)
+  return dice1 * dice2
