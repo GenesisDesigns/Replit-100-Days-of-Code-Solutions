16568948bd0cadcc4f2d27bcf20ada565e970191
GenesisDesigns <140233360+GenesisDesigns@users.noreply.github.com>
2023-09-18 14:42:25 -0500

    Update Day26_Libraries_v1.py

--- Day26/Day26_Libraries_v1.py
+++ Day26/Day26_Libraries_v1.py
@@ -1,10 +1,13 @@
 #Title: Replit Day  - Day26 - Libraries - The Power Of Libraries In Games - v1
 
+
 from replit import audio
 import os, time
 
+
 def play():
   source = audio.play_file('audio.wav')
+
 
 MENU = ('------------------------', '----| Music Player |----',
         '--- Press 1 to play ----', '--- Press 2 to pause ---',
@@ -13,12 +16,18 @@ MENU = ('------------------------', '----| Music Player |----',
 while True:
   os.system("clear")
   for line in MENU:
-
     time.sleep(0.1)
     print(line)
   print()
-
   while True:
     choice = int(input("press 1 to play or press 2 to stop: "))
     if choice == 1:
       play()
+    elif choice == 2:
+      os.system("clear")
+      exit()
+    elif choice not in [1, 2]:
+      print("invalid choice. Tryagain")
+      continue
+    else:
+      exit()
