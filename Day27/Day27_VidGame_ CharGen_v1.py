7b6dd88d3875a5c68ad0911411aab265dbee6def
GenesisDesigns <140233360+GenesisDesigns@users.noreply.github.com>
2023-09-20 10:20:07 -0500

    Update Day27_VidGame_ CharGen_v1.py

--- Day27/Day27_VidGame_ CharGen_v1.py
+++ Day27/Day27_VidGame_ CharGen_v1.py
@@ -24,3 +24,15 @@ MENU = (
   "- May the Luck be with you!        -",
   "-----------------------------------"
 )
+
+def character_backgrounds():
+  #list of char traits 
+  char_faction = ["Elf", "Human", "Orc", "Demon", "Troll", "Beast", "Shadow", "Celestials", "DemiGod"]
+  char_specialization = ["Elementals", "Thief", "Warrior", "Swordsman", "Ninja", "Magicians", "Necromancer", "Blacksmith","Assassin", "Angel", "Devil"]
+
+  #generates random char trait 
+  random_char_faction = random.choice(char_faction)
+  random_char_specialization = random.choice(char_specialization)
+
+  print(f"Character Faction: {random_char_faction}")
+  print(f"Character Specialization: {random_char_specialization}")
