5e99ebca9494d1a67aa700b93af21b9fea6e20f8
GenesisDesigns <140233360+GenesisDesigns@users.noreply.github.com>
2023-09-20 10:19:36 -0500

    Update Day27_VidGame_ CharGen_v1.py

--- Day27/Day27_VidGame_ CharGen_v1.py
+++ Day27/Day27_VidGame_ CharGen_v1.py
@@ -1,2 +1,16 @@
 #Title: Replit Day  - Day27 - Character Generator - Video Game Characters - v1
 
+""" 
+Requirements:
+1. first name and character type (human, elf, wizard, orc) 
+2. must capitalize the first letter of each word
+3. color the name and color the hp, strength
+4. dice rolls (6, 8, 12 sides)
+5. must be wrapped in a loop to ask if you want to generate a new character 
+
+Formulas: 
+  HP = (6SidedDiceRoll x 12SidedDiceRoll)/2 + 10
+  str =  (6SidedDiceRoll x 8SidedDiceRoll)/2 + 12
+
+
+"""
