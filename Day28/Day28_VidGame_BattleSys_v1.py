365ac5b9bfe414fe9d69ddb9b5b8bbf549851a26
GenesisDesigns <140233360+GenesisDesigns@users.noreply.github.com>
2023-09-22 22:08:27 -0500

    Update Day28_VidGame_BattleSys_v1.py

--- Day28/Day28_VidGame_BattleSys_v1.py
+++ Day28/Day28_VidGame_BattleSys_v1.py
@@ -1,3 +1,9 @@
 #Title: Replit Day  - Day28 - Character Battle System - Epic Character Battle - v1
 
 import os, time, random
+
+MENU = ("------------| Battle Simulator |----------",
+        "- Battle to the death!                   -",
+        "- Character with most pts wins the game  -",
+        "- May the Luck be with you!              -",
+        "-------------------------------------------")
