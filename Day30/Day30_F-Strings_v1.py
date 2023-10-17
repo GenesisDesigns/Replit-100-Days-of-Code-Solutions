d67e1509d8a75704f4cc57e3a1dfe9bc8031f08b
GenesisDesigns <140233360+GenesisDesigns@users.noreply.github.com>
2023-10-17 14:28:29 -0500

    Update Day30_F-Strings_v1.py

--- Day30/Day30_F-Strings_v1.py
+++ Day30/Day30_F-Strings_v1.py
@@ -1,21 +1,24 @@
 #Title: Replit Day  - Day30 - F-String - F...What?! - v1
 
+
 print("Survey and Feedback")
 print("30 days down - what did you think?")
 
+
 def scores_day1(score):
-    align_center = 70
-    if score > 5:
-        print(f"{'You thought it was amazing!' : ^{align_center}}")
-    elif score == 5:
-        print(f"{'you thought it was OK?' : ^{align_center}}")
-    else:
-        print(f"{'you thought it was bad?!' : ^{align_center}}")
+  align_center = 70
+  if score > 5:
+    print(f"{'You thought it was amazing!' : ^{align_center}}")
+  elif score == 5:
+    print(f"{'you thought it was OK?' : ^{align_center}}")
+  else:
+    print(f"{'you thought it was bad?!' : ^{align_center}}")
+
 
 days = 1
 for days in range(30 + 1):
-      print(f"Day {days}:")
-      day = input("What did you think about the lessons? (scale 1 to 10): ")
-      scores_day1(int(day))
-      print()
-      continue
+  print(f"Day {days}:")
+  day = input("What did you think about the lessons? (scale 1 to 10): ")
+  scores_day1(int(day))
+  print()
+  continue
