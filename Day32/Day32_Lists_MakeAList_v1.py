20ad7b7522a39b56ec7b42165d0d23560c05507f
GenesisDesigns <140233360+GenesisDesigns@users.noreply.github.com>
2023-11-04 13:00:59 -0500

    Update Day32_Lists_MakeAList_v1.py

--- Day32/Day32_Lists_MakeAList_v1.py
+++ Day32/Day32_Lists_MakeAList_v1.py
@@ -2,3 +2,11 @@
 
 
 import random
+
+#say hello in different languages
+
+greetings_list = [
+  'السلام عليكم', '你好', 'Hallo', 'Bonjour', 'Guten Tag', 'नमस्ते', 'Ciao',
+  'こんにちは', '안녕하세요', 'Cześć', 'Olá', 'Здравствуйте', 'Hola', 'Hej', 'สวัสดี',
+  'Merhaba', 'Вітаю', 'Xin chào', 'Helô', 'Bawo ni'
+]
